ea31909fd72709c65bd507fc44c2989f9c113c21
keerthivasanA01 <akeerthi620@gmail.com>
2025-01-22 07:49:19 +0530

    l

--- FedEX Hackathon/Dynamic_Routing_Emission_Calculator/schema.sql
+++ FedEX Hackathon/Dynamic_Routing_Emission_Calculator/schema.sql
@@ -4,7 +4,8 @@ USE route_optimizer;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
