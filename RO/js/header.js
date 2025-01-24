e4cefaeb9a7a77e1407a941319af288fb7f70ef6
Arthi <119476304+ArthiRADS@users.noreply.github.com>
2025-01-24 21:28:50 +0530

    Add files via upload

---
+++ RO/js/header.js
@@ -0,0 +1,13 @@
+document.getElementById('header').innerHTML = `
+  <nav class="navbar">
+    <div class="container">
+      <a href="index.html" class="logo">FedEx Dynamic Routing System</a>
+      <ul class="nav-links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="optimize-route.html">Optimize Route</a></li>
+        <li><a href="emissions-dashboard.html">Emissions Dashboard</a></li>
+      </ul>
+      <a href="login.html" class="btn login-btn">Login</a>
+    </div>
+  </nav>
+`;
